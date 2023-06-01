866355e60b6b02dd87b55caf1e17ee5f2a0007a1
Harsh0897kumar <114602803+Harsh0897kumar@users.noreply.github.com>
2023-06-02 02:13:16 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -24,12 +24,12 @@ async def allowed(_, __, message):
 async def gen_link_s(bot, message):
     replied = message.reply_to_message
     if not replied:
-        return await message.reply('SʜᴀʀᴇAʙʟᴇ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴇ Pᴀɴɴᴜʀᴀᴛʜᴜᴋᴜ Eᴛʜᴀᴄʜɪ Mᴇssᴀɢᴇ Kᴜ Rᴇᴘʟʏ Pᴀɴɴᴜɴɢᴀ.')
+        return await message.reply('Rᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇꜱꜱᴀɢᴇ ᴛᴏ ɢᴇᴛ ᴀ ꜱʜᴀʀᴇᴀʙʟᴇ ʟɪɴᴋ.')
     file_type = replied.media
     if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
-        return await message.reply("Sᴜᴘᴘᴏʀᴛ Aɢᴜʀᴀ Fᴏʀᴍᴀᴛ Kᴜ Mᴀᴛᴛᴜᴍ Rᴇᴘʟʏ Pᴀɴɴᴜɴɢᴀ")
+        return await message.reply("ʀᴇᴘʟʏ ᴛᴏ sᴜᴘᴘᴏʀᴛᴇᴅ ᴍᴇᴅɪᴀ")
     if message.has_protected_content and message.chat.id not in ADMINS:
-        return await message.reply("okDa")
+        return await message.reply("This Message is not for you dear")
     file_id, ref = unpack_new_file_id((getattr(replied, file_type.value)).file_id)
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
@@ -40,10 +40,10 @@ async def gen_link_s(bot, message):
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TeamEvamaria/10 https://t.me/TeamEvamaria/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Get_Update_Official https://t.me/Get_Update_Official</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TeamEvamaria/10 https://t.me/TeamEvamaria/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Get_Update_Official https://t.me/Get_Update_Official</code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
@@ -63,17 +63,17 @@ async def gen_link_batch(bot, message):
         l_chat_id  = int(("-100" + l_chat_id))
 
     if f_chat_id != l_chat_id:
-        return await message.reply("Cʜᴀᴛ ɪᴅs Mᴀᴛᴄʜ Aɢᴀʟᴀ.")
+        return await message.reply("ᴄʜᴀᴛ ɪᴅ's ɴᴏᴛ ᴍᴀᴛᴄʜᴇᴅ.")
     try:
         chat_id = (await bot.get_chat(f_chat_id)).id
     except ChannelInvalid:
-        return await message.reply('Iɴᴛʜᴀ Cʜᴀɴɴᴇʟ / Gʀᴏᴜᴘ Pʀɪᴠᴀᴛᴇ Aʜ Iʀᴜᴋᴀʟʟᴀᴍ. Pʀɪᴠᴀᴛᴇ Cʜᴀɴɴᴇʟ / Gʀᴏᴜᴘ Yᴇɴɴᴀ Aᴅᴍɪɴ Aᴋɪ Fɪʟᴇs Iɴᴅᴇx Pᴀɴɴɪᴋᴏɴɢᴀ')
+        return await message.reply('Tʜɪꜱ ᴍᴀʏ ʙᴇ ᴀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ / ɢʀᴏᴜᴘ. Mᴀᴋᴇ ᴍᴇ ᴀɴ ᴀᴅᴍɪɴ ᴏᴠᴇʀ ᴛʜᴇʀᴇ ᴛᴏ ɪɴᴅᴇx ᴛʜᴇ ғɪʟᴇꜱ.')
     except (UsernameInvalid, UsernameNotModified):
         return await message.reply('Invalid Link specified.')
     except Exception as e:
         return await message.reply(f'Errors - {e}')
 
-    sts = await message.reply("Uɴɢᴀ Mᴇssᴀɢᴇ Kᴜ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴇ Pᴀɴɴᴜʀᴏᴍ.\nTʜɪs ᴍᴀʏ ᴛᴀᴋᴇ ᴛɪᴍᴇ ᴅᴇᴘᴇɴᴅɪɴɢ ᴜᴘᴏɴ ɴᴜᴍʙᴇʀ ᴏғ ᴍᴇssᴀɢᴇs")
+    sts = await message.reply("ɢᴇɴᴇʀᴀᴛɪɴɢ ʟɪɴᴋ's ғᴏʀ ʏᴏᴜʀ ᴍᴇssᴀɢᴇ.\nTʜɪꜱ ᴍᴀʏ ᴛᴀᴋᴇ ᴛɪᴍᴇ ᴅᴇᴘᴇɴᴅɪɴɢ ᴜᴘᴏɴ ɴᴜᴍʙᴇʀ ᴏғ ᴍᴇꜱꜱᴀɢᴇꜱ")
     if chat_id in FILE_STORE_CHANNEL:
         string = f"{f_msg_id}_{l_msg_id}_{chat_id}_{cmd.lower().strip()}"
         b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
