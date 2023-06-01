fe2a5500d70eeae06ebe57f196401a59868b8cf7
Harsh0897kumar <114602803+Harsh0897kumar@users.noreply.github.com>
2023-06-02 03:35:07 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -13,7 +13,7 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='Uɴɢᴀ Mᴇssᴀɢᴇ Aʜ Uɴɢᴀ Bᴏᴛ Usᴇʀs Kᴜ Sᴇɴᴅ Pᴀɴɴɪᴛᴜ Iʀᴜᴋᴜ...'
+        text='ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ᴜʀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʜɪs ʙᴏᴛ ᴜsᴇʀs...'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
