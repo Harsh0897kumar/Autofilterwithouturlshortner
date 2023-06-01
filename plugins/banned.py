7293f3e4e350f084cb7d964b6e4d6160d9239865
Harsh0897kumar <114602803+Harsh0897kumar@users.noreply.github.com>
2023-06-02 03:38:01 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -21,7 +21,7 @@ disabled_group=filters.create(disabled_chat)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'Kᴀᴛʜᴀᴍ,Gᴀʏᴀ,Tᴀᴛᴀ,Gᴏᴏᴅ Bʏᴇ :- Cᴏɴᴛᴀᴄᴛ Aᴅᴍɪɴ 😉 \nBan Reason: {ban["ban_reason"]}')
+    await message.reply(f'sᴏʀʀʏ ᴜ ʀᴇ ʙᴀɴɴᴇᴅ ʙʏ ᴀᴅᴍɪɴ. \nBan Reason: {ban["ban_reason"]}')
 
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
@@ -31,7 +31,7 @@ async def grp_bd(bot, message):
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
     k = await message.reply(
-        text=f"CHAT NOT ALLOWED 🐞\n\n.Kᴀᴛʜᴀᴍ,Gᴀʏᴀ,Tᴀᴛᴀ,Gᴏᴏᴅ Bʏᴇ :- Cᴏɴᴛᴀᴄᴛ Aᴅᴍɪɴ 😉\nReason : <code>{vazha['reason']}</code>.",
+        text=f"ᴄʜᴀᴛ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ 🐞\n\nᴜ ʜᴀᴠᴇ ʙᴀɴɴᴇᴅ ᴅᴜᴇ ᴛᴏ ɪʟʟᴇɢᴀʟ ᴜsᴀɢᴇ ᴏғ ᴍᴇ .. ɪғ ᴜ ᴜsᴇ ᴍᴇ ɴᴏᴡ ᴀғᴛᴇʀ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ sᴜᴘᴘᴏʀᴛ ᴏʀ ᴏᴡɴᴇʀ \nReason : <code>{vazha['reason']}</code>.",
         reply_markup=reply_markup)
     try:
         await k.pin()
